f01a266cf907c146a0d13c305ef68397bef9624d
giovannilampis <giolampis@hotmail.com>
2023-06-01 15:41:40 +0200

    define a constructor inside class Product

--- models/product.php
+++ models/product.php
@@ -7,6 +7,15 @@ class Product
     public $price;
     public $description;
 
+    public function __construct($_image, $_name, $_price, $_description)
+    {
+
+        $this->image = $_image;
+        $this->name = $_name;
+        $this->price = $_price;
+        $this->description = $_description;
+    }
+
 }
 
 ?>
