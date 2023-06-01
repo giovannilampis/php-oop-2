5b4c97eadf9ec26346cd283c59b5a0b4737ea04e
giovannilampis <giolampis@hotmail.com>
2023-06-01 16:29:12 +0200

    4 instances of class Movie in partials/product.php

--- views/partials/product.php
+++ views/partials/product.php
@@ -2,4 +2,18 @@
 
 include __DIR__ . '/../../models/product.php';
 
+$product_0001 = new Product('https://unsplash.com/it/foto/w6elADh_jww', 'croquettes', '10', 'Best quality croquettes for the pet you love');
+
+$product_0002 = new Product('https://unsplash.com/it/foto/ZJsM3GRdAnY', 'pate', '18', 'Best quality pate for the pet you need to pamper');
+
+$product_0002 = new Product('https://unsplash.com/it/foto/A7nK49HCqSI', 'pet bed', '70', 'Make sure your pet to have the best sleep time');
+
+$product_0003 = new Product('https://unsplash.com/it/foto/q9iyCG7FRmQadd $movieA & $movieB, instances of class Movie', 'game', '8', 'Let your pet to have the best games to play with');
+
 ?>
+
+<main>
+
+
+
+</main>
