61e006fb185779166ae64ec2b59c84d37831aec6
giovannilampis <giolampis@hotmail.com>
2023-06-01 17:27:22 +0200

    dinamically print cards in browser

--- views/partials/product.php
+++ views/partials/product.php
@@ -9,33 +9,36 @@ $arrayProducts = [
     new Product('https://images.unsplash.com/photo-1589924749359-9697080c3577?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80', 'game', '8', 'Let your pet to have the best games to play with')
 ];
 
-$product_0000 = new Product('https://images.unsplash.com/photo-1568640347023-a616a30bc3bd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1473&q=80', 'croquettes', '10', 'Best quality croquettes for the pet you love');
-
-$product_0001 = new Product('https://unsplash.com/it/foto/ZJsM3GRdAnY', 'pate', '18', 'Best quality pate for the pet you need to pamper');
-
-$product_0002 = new Product('https://unsplash.com/it/foto/A7nK49HCqSI', 'pet bed', '70', 'Make sure your pet to have the best sleep time');
-
-$product_0003 = new Product('https://images.unsplash.com/photo-1589924749359-9697080c3577?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80', 'game', '8', 'Let your pet to have the best games to play with');
-
 ?>
 
 <main class="container">
 
-    <div class="card" style="width: 18rem;">
-        <img src="<?php echo $product_0000->image ?>" class="card-img-top" alt="<?php echo $product_0000->name ?>">
-        <div class="card-body">
-            <h5 class="card-title">
-                <?php echo $product_0000->name ?>
-            </h5>
-            <p class="card-text">
-                <?php echo $product_0000->description ?>
-            </p>
-            <h5 class="card-title">
-                <?php echo $product_0000->price ?>
-            </h5>
-            <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Go somewhere</a>
-        </div>
+    <div class="row">
+
+        <div class="col-6">
+            <?php foreach ($arrayProducts as $element) { ?>
+                <div class="card" style="width: 18rem;">
+                    <img src="<?php echo $element->image ?>" class="card-img-top" alt="<?php echo $element->name ?>">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <?php echo $element->name ?>
+                        </h5>
+                        <p class="card-text">
+                            <?php echo $element->description ?>
+                        </p>
+                        <h5 class="card-title">
+                            <?php echo $element->price ?>
+                        </h5>
+                        <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Go
+                            somewhere</a>
+                    </div>
+                </div>
+
+            </div>
+        <?php } ?>
+
     </div>
+
 
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
