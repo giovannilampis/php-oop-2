3997beebcc4c58a46cb714ade86a8e979449b3fc
giovannilampis <giolampis@hotmail.com>
2023-06-02 09:12:44 +0200

    fix bug in modal

--- views/partials/product.php
+++ views/partials/product.php
@@ -9,54 +9,67 @@ $arrayProducts = [
     new Product('https://images.unsplash.com/photo-1589924749359-9697080c3577?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80', 'game', '8', 'Let your pet to have the best games to play with')
 ];
 
+$clicked_product_number = 1;
+
 ?>
 
 <main class="container">
 
     <div class="row">
 
-        <div class="col-6">
-            <?php foreach ($arrayProducts as $element) { ?>
-                <div class="card" style="width: 18rem;">
-                    <img src="<?php echo $element->image ?>" class="card-img-top" alt="<?php echo $element->name ?>">
+        <?php for ($i = 0; $i < count($arrayProducts); $i++) { ?>
+            <div class="col-6">
+                <div class="card">
+                    <img src="<?php echo $arrayProducts[$i]->image ?>" class="card-img-top"
+                        alt="<?php echo $arrayProducts[$i]->name ?>">
                     <div class="card-body">
                         <h5 class="card-title">
-                            <?php echo $element->name ?>
+                            <?php echo $arrayProducts[$i]->name ?>
                         </h5>
                         <p class="card-text">
-                            <?php echo $element->description ?>
+                            <?php echo $arrayProducts[$i]->description ?>
                         </p>
                         <h5 class="card-title">
-                            <?php echo $element->price ?>
+                            <?php echo $arrayProducts[$i]->price ?>
                         </h5>
-                        <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Go
-                            somewhere</a>
+                        <a onclick=" <?php $clicked_product_number = $i ?> " href="" class="btn btn-primary"
+                            data-bs-toggle="modal" data-bs-target="<?php echo "#product" . $i ?>">
+                            <?php echo $arrayProducts[$i]->name ?>
+                        </a>
+                        <span>
+                            INDEX:
+                            <?php echo $i ?>
+                        </span>
                     </div>
                 </div>
 
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="<?php echo "product" . $i ?>" tabindex="-1" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">
+                                <?php echo $arrayProducts[$i]->name ?>
+                            </h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         <?php } ?>
 
     </div>
 
 
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </div>
-    </div>
 
 </main>
