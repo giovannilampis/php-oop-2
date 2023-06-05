2de9eb0f792112310b0e4f3a41c8da5282e20229
giovannilampis <giolampis@hotmail.com>
2023-06-05 16:16:09 +0200

    add public function setPrice($_price)

--- models/product.php
+++ models/product.php
@@ -12,12 +12,22 @@ class Product
     {
         $this->image = $_image;
         $this->name = $_name;
-        $this->price = $_price;
+        // $this->price = $_price;
+        $this->setPrice($_price);
         $this->description = $_description;
     }
     public function get_product_details()
     {
         return " This product is called: $this->name, it cost € $this->price ";
+    }
+
+    public function setPrice($_price)
+    {
+        if ($_price <= 0) {
+            throw new Exception('Invalid price for product: $this->name');
+        } else {
+            return $_price;
+        }
     }
 }
 class ProductAnimal extends Product
